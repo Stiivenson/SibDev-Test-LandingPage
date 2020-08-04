a7f00a022d342798f05ad3f724d256a4a5a98222
Stiivenson <nicklihorus4@gmail.com>
2020-08-04 18:35:50 +0700

    add standart files config + basic template

--- gulpfile.js
+++ gulpfile.js
@@ -16,26 +16,19 @@ const uglify = require("gulp-uglify");
 const concat = require("gulp-concat");
 const babel = require("gulp-babel");
 
-const imagemin = require('gulp-imagemin');
-const imageminMozjpeg = require('imagemin-mozjpeg');
-const imageminJpegRecompress = require('imagemin-jpeg-recompress');
-const imageminPngquant = require('imagemin-pngquant');
 
 var path = {
     build: {
-        css: './website/html+css+js/',
-        js: './website/html+css+js/js/',
-        ready_img: "./website/img-optimization/ready"
+        css: './website/',
+        js: './website/'
     },
     src: {
-        css: "./website/html+css+js/css/template.+(scss|sass)",
-        js: "./website/html+css+js/js/src/*.js",
-        raw_img: "./website/img-optimization/raw/*"
+        css: "./website/style/scss/template.+(scss|sass)",
+        js: "./website/js/*.js"
     },
     watch: {
-        css: "./website/html+css+js/css/**/*.scss",
-        js: "./website/html+css+js/js/src/*.js",
-        raw_img: "./website/img-optimization/raw/*"
+        css: "./website/style/scss/**/*.scss",
+        js: "./website/js/*.js"
     }
 }
 
@@ -61,36 +54,18 @@ function css() {
 
 function js() {
     return src(path.src.js)
-        .pipe(concat('template.min.js'))
+        .pipe(concat('template.js'))
         .pipe(babel({
             presets: ['@babel/env']
         }))
-        .pipe(uglify())
         .pipe(dest(path.build.js))
-}
-
-function mozjpeg() {
-    return src(path.src.raw_img)
-        .pipe(imagemin([
-            imageminJpegRecompress({
-                progressive: true,
-                max: 80,
-                min: 70
-            }),
-            imageminPngquant({
-                quality: [0.8, 1]
-            })
-        ]))
-        .pipe(dest(path.build.ready_img))
 }
 
 function watchCss() {
     gulp.watch(path.watch.css, gulp.series('css'));
     gulp.watch(path.watch.js, gulp.series('js'));
-    gulp.watch(path.watch.raw_img, gulp.series('mozjpeg'));
 }
 
 exports.css = css;
 exports.js = js;
-exports.mozjpeg = mozjpeg;
 exports.watch_css = watchCss;
